0340249a6ffe39977993496b55dcbaee65cddb41
Allan <a.callaghan@civicboom.com>
2011-06-19 14:15:55 +0100

    added freeminder push

---
+++ hActivate/hactivate/lib/push_to_services.py
@@ -0,0 +1,31 @@
+import json, httplib
+
+
+def freeminder(item):
+    #http://www.free-minder.org/rest/offer
+    
+    if not item:
+        return False
+    
+    body = json.dumps({
+        'sender'       : item.user.username ,
+        'groupID'      : 'FreeHoc'          ,
+        'location_name': 'not implemented'  ,
+        'location_lat' : str(item.lat)      ,
+        'location_lon' : str(item.lon)      ,
+        'title'        : item.title         ,
+        'message'      : item.description   ,
+        'item_id'      : str(item.id)       ,
+    })
+    
+    connection = httplib.HTTPConnection("www.free-minder.org")
+    
+    headers = {'Content-Type': 'application/json'}
+    
+    connection.request("POST", "http://www.free-minder.org/rest/offer" ,headers=headers, body=body)
+    
+    resp = connection.getresponse()
+    data = resp.read()
+    
+    print data, resp.status, resp.reason
+    return resp.status==200
