41e9ec013f9eb2e79ef32677387848abfe49a14e
Greg Miell <greg@greg-net.co.uk>
2011-06-19 12:58:55 +0100

    Wooo, sending SMS

--- BlueVia Testing/test.py
+++ BlueVia Testing/test.py
@@ -15,7 +15,8 @@ authUrl         = "https://connect.bluevia.com/authorise"
 oauthUrls       = {
                    'request' : oauthBaseUrl+'Oauth/getRequestToken',
                    'access'  : oauthBaseUrl+'Oauth/getAccessToken',
-                   'sms'     : oauthBaseUrl+'SMS_Sandbox/inbound/445480605/messages'+"?version=v1&alt=json"
+                   'sms'     : oauthBaseUrl+'SMS_Sandbox/inbound/445480605/messages'+"?version=v1&alt=json",
+                   'smsSend' : oauthBaseUrl+'SMS/outbound/requests'+"?version=v1",
                   }
 
 appid           = "468f6918612d54133e8a0dcab2fff79b"
@@ -23,8 +24,8 @@ appname         = "FreeHocTest"
 key             = "HI11061838986678"
 secret          = "QucX67152640"
 
-token           = "1601c3501b79c84c6819cd2b792c087d"
-token_secret    = "d9e368e4525b980569e2676b47d55d2e"
+token           = "1cff2685c6ef5e4c7f58360077d21694"
+token_secret    = "ac28ce7e496326ea66a030551cbb5e31"
 
 mokeyword       = "TESTHOC"
 sandkeyword     = "SANDHOC"
@@ -35,16 +36,29 @@ consumer        = oauth.Consumer(key=key, secret=secret)
 
 token           = oauth.Token(key=token, secret=token_secret)
 
-request         = oauth.Request.from_consumer_and_token(consumer, token, "GET", oauthUrls['sms'])#, parameters={'version':'v1', 'alt':'json'})
-
-request.sign_request(sig_hmac, consumer, token)
-
-print sig_hmac.check(request, consumer, token, request.get_parameter('oauth_signature'))
 
 #assert False
 
+body = {"smsText": {
+            "address":          {"phoneNumber":"447928065717"},
+            "message":          "Message here",
+            "originAddress":    {"alias": token.key},
+            },
+        }
+
+request         = oauth.Request.from_consumer_and_token(consumer, token, "POST", oauthUrls['smsSend'])#, parameters={'version':'v1', 'alt':'json'})
+request.sign_request(sig_hmac, consumer, token)    
+
+body = json.dumps(body)
 connection = httplib.HTTPSConnection("api.bluevia.com")
-connection.request("GET", oauthUrls['sms'], headers=request.to_header(realm="https://api.bluevia.com"))
+
+headers = request.to_header(realm="https://api.bluevia.com")
+
+headers.update({'Content-Type': 'application/json'});
+
+connection.request("POST", oauthUrls['smsSend'],headers=headers, body=body)
 
 resp = connection.getresponse()
-print  resp.read()
+data = resp.read()
+
+print data, resp.status, resp.reason
